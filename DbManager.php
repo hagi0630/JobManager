e9667738fa224898ba9b07318b05b100b775a542
hagi0630 <60420068+hagi0630@users.noreply.github.com>
2022-06-30 21:26:16 +0900

    Update DbManager.php

--- DbManager.php
+++ DbManager.php
@@ -3,7 +3,7 @@
 function connect(){
     $dsn = "mysql:dbname=job;host=localhost;charset=utf8";
     $usr = "root";
-    $passwd = "hagi";
+    $passwd = "*****";
 
     try{
     $db = new PDO($dsn,$usr,$passwd);
